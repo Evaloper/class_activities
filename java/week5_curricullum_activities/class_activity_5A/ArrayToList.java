11a965558a39ac8253522d881f95bff590d5ef64
evaloper <gbajeeva@yahoo.com>
2024-02-21 11:25:41 +0100

    C0mmit

--- java/week5_curricullum_activities/class_activity_5A/ArrayToList.java
+++ java/week5_curricullum_activities/class_activity_5A/ArrayToList.java
@@ -1,11 +1,24 @@
 package week5_curricullum_activities.class_activity_5A;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ArrayToList {
     public static void main(String[] args) {
         String[] stack = {"Java", ".net"};
 
         System.out.println("Arrays: " + Arrays.toString(stack));
+
+        System.out.println("******************");
+        ArrayList<String> arr = new ArrayList<>(Arrays.asList(stack));
+        System.out.println(arr);
+
+        System.out.println("******************");
+        List<String> list = new ArrayList<>();
+        list.add(Arrays.toString(stack));
+        for (String li : list){
+            System.out.println(li);
+        }
     }
 }
